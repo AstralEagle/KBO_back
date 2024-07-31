f522499af946cc43b904e8f45181ba7042d2e833
antoineRchd <antoine.richard91@gmail.com>
2024-07-31 14:42:09 +0200

    back connexion

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const connectDB = require('./db');
-const app = express();
-const port = 3001;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-// Connect to MongoDB
-connectDB();
+dotenv.config();
+
+const app = express();
 
 // Middleware
 app.use(express.json());
 
-// Simple route for testing
-app.get('/api', (req, res) => {
-  res.json({ message: 'Backend is connected!' });
-});
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 // Routes
-app.use('/api/companies', require('./routes/companies'));
+app.use('/api/users', require('./routes/userRoutes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
